d8840f4fdac5b23e136401ad81a184d2d734a437
Vadim Glushchenko <maliken.webdev@gmail.com>
2021-01-03 00:20:04 +0300

    change cors settings

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 
 export const bootstrap = (app: Express) => {
   dotenv.config();
-  app.use(cors({ origin: process.env.CORS_ORIGIN || "http://localhost" }));
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN || "http://localhost",
+      credentials: true,
+    })
+  );
   app.use(express.json());
 };
